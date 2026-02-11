b93ed84f4ff967e08ade792070fe0c109e0c9992
ezequielsanchez-canul <ezequielsanchezcursos@gmail.com>
2026-02-10 23:57:28 -0500

    Agrego el ultimo punto de comprobar si tiene numeros iguales y mostrar mensaje en la consola

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ Crear un programa en Javascript que realice lo siguiente:
 - Debe solicitar al usuario 3 números por prompt y guardarlos en sus respectivas variables.
 - Debe analizar los números, identificar cual es el número mayor, el número del centro y el número menor.
 - Debe imprimir los números por consola o por el DOM ordenados de mayor a menor, y de menor a mayor.
-
 - Debe ser capaz de identificar si los números son iguales e imprimir un mensaje por consola o por el DOM diciendo que los números son iguales.
 
 Prueba tu programa con las siguientes entradas para asegurarte que funcione correctamente:
@@ -37,25 +36,22 @@ let maximo = Math.max(num1, num2, num3);
 let min = Math.min(num1, num2, num3);
 let medio = num1 + num2 + num3 - maximo - min
 
-if(num1 == num2 || num1 == num3 || num2 == num3){
+if(num1 === num2 || num1 === num3 || num2 === num3){
     console.log("Hay numeros iguales")
-}else{
-return{maximo, medio, min};
-}//else
-
 }
+return{maximo, medio, min};
+}//ordenDes
 
 function ordenAsc(num1,num2,num3) {
 let maximo = Math.max(num1, num2, num3);
 let min = Math.min(num1, num2, num3);
 let medio = num1 + num2 + num3 - maximo - min
 
+if(num1 === num2 || num1 === num3 || num2 === num3){
+    console.log("Hay numeros iguales")
+}//if
 return{min, medio, maximo};
-}
-
-
-/* const resultado = ordenNum(num1, num2, num3); */
+}//ordenAsc
 
 console.log(ordenDes(num1, num2, num3));
-
 console.log(ordenAsc(num1, num2, num3));
